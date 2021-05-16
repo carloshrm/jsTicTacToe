164507808733b0b69be3777f391104d078b1b828
cm <cm@CM-PC>
2021-05-16 10:32:32 -0300

    win message

--- game.js
+++ game.js
@@ -1,14 +1,16 @@
 const clearButton = document.getElementById("reset");
+const boardSquares = document.querySelectorAll(".board_spot");
+const board_HTML = document.getElementById("game_board");
+const name_input = document.querySelectorAll(".player_in");
+const result_out = document.getElementById("results");
 
 const Board = () => {
-  const boardSquares = document.querySelectorAll(".board_spot");
-  const board_HTML = document.getElementById("game_board");
-
   const spots = {};
   const clearBoard = () => {
-    boardSquares.forEach((sqr) => (sqr.innerHTML.replace = ""));
-    nameInput.forEach((x) => (x.value = ""));
+    boardSquares.forEach((sqr) => (sqr.innerHTML = ""));
+    name_input.forEach((x) => (x.value = ""));
   };
+  clearButton.addEventListener("click", clearBoard);
   const getIcon = (target, type) => {
     const cross = `
       <div class="icon" id="right"></div>
@@ -19,17 +21,16 @@ const Board = () => {
       `;
     target.innerHTML = type === "x" ? cross : circle;
   };
-  return { spots, clearBoard, getIcon, board_HTML };
+  return { spots, getIcon, board_HTML };
 };
 
 const Players = () => {
-  const nameInput = document.querySelectorAll(".player_in");
   const p1 = {
-    name: nameInput[0].value !== "" ? nameInput[0].value : "John Doe",
+    name: name_input[0].value !== "" ? name_input[0].value : "John Doe",
     icon: "x",
   };
   const p2 = {
-    name: nameInput[1].value !== "" ? nameInput[1].value : "Mike Doe",
+    name: name_input[1].value !== "" ? name_input[1].value : "Mike Doe",
     icon: "o",
   };
   return { p1, p2 };
@@ -47,12 +48,10 @@ const Game = () => {
     board.getIcon(e.target, currentPlayer.icon);
     board.spots[e.target.id] = currentPlayer.icon;
     console.log(board.spots);
-    checkWin();
+    checkWin(currentPlayer);
   };
-  const checkWin = () => {
-    // 0 1 2
-    // 3 4 5
-    // 6 7 8
+  board.board_HTML.addEventListener("click", makePlay);
+  const checkWin = (lastPlayer) => {
     const winCases = [
       [0, 1, 2],
       [3, 4, 5],
@@ -63,22 +62,19 @@ const Game = () => {
       [0, 4, 8],
       [2, 4, 6],
     ];
-
-    winCases.forEach((x) => {
-      if (board.spots[x[0]] === undefined) return;
+    winCases.forEach((caseValue) => {
+      if (board.spots[caseValue[0]] === undefined) return;
       if (
-        board.spots[x[0]] === board.spots[x[1]] &&
-        board.spots[x[0]] === board.spots[x[2]]
+        board.spots[caseValue[0]] === board.spots[caseValue[1]] &&
+        board.spots[caseValue[0]] === board.spots[caseValue[2]]
       ) {
-        alert("win");
+        result_out.innerText = `${lastPlayer.name} wins!`;
       }
     });
   };
-  board.board_HTML.addEventListener("click", makePlay);
+
   return { board, players };
 };
-
-function startUp() {
+(() => {
   const currentGame = Game();
-}
-startUp();
+})();
