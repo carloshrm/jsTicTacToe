2e0a758c500304a6c5480794a5edfcee2dc27d90
chm <carloshrmoraes@gmail.com>
2021-05-18 09:57:49 -0300

    refactor

--- game.js
+++ game.js
@@ -8,7 +8,33 @@ const dom = {
 
 const Board = () => {
   let spots = {};
-  const plays = 0;
+  let plays = 0;
+
+  const checkWin = () => {
+    const winCases = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    plays++;
+    if (plays === 9) {
+      return 'draw';
+    }
+    for (let i = 0; i < winCases.length; i++) {
+      if (spots[winCases[i][0]] === undefined) continue;
+      if (
+        spots[winCases[i][0]] === spots[winCases[i][1]] &&
+        spots[winCases[i][0]] === spots[winCases[i][2]]
+      ) {
+        return 'win';
+      }
+    }
+  };
 
   const setIcon = (target, type) => {
     const cross = `
@@ -21,82 +47,69 @@ const Board = () => {
     target.innerHTML = type === 'x' ? cross : circle;
   };
 
-  return { spots, setIcon, plays };
+  return { spots, setIcon, plays, checkWin };
 };
 
 const Players = () => {
   const p1 = {
-    name: dom.name_input[0].value !== '' ? name_input[0].value : 'John Doe',
+    name: dom.name_input[0].value,
     icon: 'x',
   };
   const p2 = {
-    name: dom.name_input[1].value !== '' ? name_input[1].value : 'Mike Doe',
+    name: dom.name_input[1].value,
     icon: 'o',
   };
-  return { p1, p2 };
+  const updateName = function () {
+    p1.name = dom.name_input[0].value;
+    p2.name = dom.name_input[1].value;
+  };
+  return { p1, p2, updateName };
 };
 
 const gameSetup = () => {
-  const playerTurn = true;
+  let playerTurn = true;
   const board = Board();
   const players = Players();
 
+  function makePlay(e) {    
+    if (e.target.className !== 'board_spot' || e.target.innerHTML !== '')
+      return;
+    let currentPlayer = playerTurn ? players.p1 : players.p2;
+    playerTurn = !playerTurn;
+    board.setIcon(e.target, currentPlayer.icon);
+    board.spots[e.target.id] = currentPlayer.icon;
+    let result = board.checkWin();
+    gameOver(result, currentPlayer.name);
+  }
+  function gameOver(result, name) {
+    if (result === 'draw') {
+      dom.result_out.innerText = "It's a draw. Reset to play again.";
+    } else if (result === 'win') {
+      dom.result_out.innerText = `${name} has won! Reset to play again.`;
+      dom.board_HTML.removeEventListener('click', makePlay);
+    }
+  }
+
+  dom.board_HTML.addEventListener('click', makePlay);
+  dom.result_out.innerText = 'Click a spot on the board to play!';
   return { playerTurn, board, players };
 };
 
 (() => {
-  const winCases = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  function makePlay(e) {
-    if (e.target.className !== 'board_spot' || e.target.innerHTML !== '')
-      return;
-    let currentPlayer = myGame.playerTurn
-      ? myGame.players.p1
-      : myGame.players.p2;
-    myGame.playerTurn = !myGame.playerTurn;
-    myGame.board.setIcon(e.target, currentPlayer.icon);
-    myGame.board.spots[e.target.id] = currentPlayer.icon;
-    console.log(myGame.board.spots);
-    checkWin(currentPlayer);
-  }
-
-  function checkWin(lastPlayer) {
-    myGame.board.plays++;
-    winCases.forEach((caseValue) => {
-      if (myGame.board.spots[caseValue[0]] === undefined) return;
-      if (
-        myGame.board.spots[caseValue[0]] === myGame.board.spots[caseValue[1]] &&
-        myGame.board.spots[caseValue[0]] === myGame.board.spots[caseValue[2]]
-      ) {
-        gameOver(lastPlayer.name);
-      }
-    });
-    if (myGame.board.plays === 9) {
-      alert('draw');
-    }
-  }
-  function gameOver(name) {
-    dom.result_out.innerText = `${name} wins!`;
-    dom.board_HTML.removeEventListener('click', makePlay);
-  }
+  let myGame;
   function reset() {
     dom.boardSquare.forEach((sqr) => (sqr.innerHTML = ''));
-    dom.name_input.forEach((x) => (x.value = ''));
-    myGame = startUp();
-  }  
-  function startUp() {
-    dom.board_HTML.addEventListener('click', makePlay);
-    return gameSetup();
+    dom.name_input.forEach((x) => (x.value = x.defaultValue));
+    dom.result_out.innerText = '';
+    myGame = gameSetup();    
   }
-  let myGame = startUp();
+  myGame = gameSetup();
   dom.clearButton.addEventListener('click', reset);
+  dom.name_input.forEach((x) =>
+    x.addEventListener('change', () => {
+      myGame.players.updateName();
+      console.log(myGame);
+    })
+  )
+
 })();
